819f599cca59b425c227118a9897bcc979ddc6a7
Raphael <raphael.teixeira@vicax.com>
2022-01-09 16:08:25 -0300

    Config and Jwt modules.

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-
   const port = 3000;
   await app.listen(port);
-
   logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
