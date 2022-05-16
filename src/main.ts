f9893afe9df8efef2eb20c221d5b71b442671053
raphaelteixeiravicax <raphael.teixeira@vamosparcelar.com.br>
2022-05-15 23:53:34 -0300

    Add bearer auth.

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
     .setTitle('Task management')
     .setDescription('Tarefas em NestJS')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
